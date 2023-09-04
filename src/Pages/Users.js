c6604ca66664fe93a4638f1d86e052327fb39cdf
ThunderStorm24 <patryko5@onet.pl>
2023-09-04 17:45:17 +0200

    Trzeba zrobic ze tylko admin ma dostep

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -41,6 +41,7 @@ function Users() {
             setAddModal(false);
     };
 
+    //Sesja czy Admin jest zalogowany, narazie jest i user i admin
     useEffect(() => {
         Axios.get("/login").then((response) => {
             if (response.data.loggedIn == true) {
